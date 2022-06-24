98e4ee9523d180f5d64188bf9b23c6577fedc51c
b0b1n <sohaibskious@gmail.com>
2022-06-24 21:02:33 +0200

    Adding events and using them

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Homepage</h2>
-        </div>
-      );
-}
- 
+  // const handleClick = ()=>{
+  //     console.log("Hello Click");
+  // }
+
+  const handleClick = (name, e) => {
+    console.log(`Hello,  ${name}`);
+    console.log(e.target);
+  };
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      {/* <button onClick={handleClick}>Click me</button> */}
+      <button onClick={(e) => handleClick("Sohaib", e)}>Click me</button>
+    </div>
+  );
+};
+
 export default Home;
