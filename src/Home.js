c4888a03e775e5d3cadcf466098b483703a8f8ca
b0b1n <sohaibskious@gmail.com>
2022-06-26 04:39:19 +0200

    Geting data from json file and displaying it

--- src/Home.js
+++ src/Home.js
@@ -2,51 +2,21 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      id: 1,
-      title:
-        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      author: "Sohaib",
-      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-    },
-    {
-      id: 2,
-      title: "qui est esse",
-      author: "Skious",
-      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-    },
-    {
-      id: 3,
-      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      author: "b0b1n",
-      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
-    },
-    {
-      id: 4,
-      title: "eum et est occaecati",
-      author: "Sohaib Skious",
-      body: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit",
-    },
-  ]);
-
-  const [name, setName] = useState("b0b1n");
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("Use effect Ran");
-    console.log(name);
-  }, [name]);
+    fetch("http://localhost:8000/blogs")
+    .then(res => {
+        return res.json();
+    })
+    .then(data =>{
+      setBlogs(data)
+    })
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
-      <button onClick={()=> setName("Skious Sohaib")}>Change Name</button>
-      {name}
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
 };
