34a6358f652b20b0275846b838dde1d5cf359cdc
b0b1n <sohaibskious@gmail.com>
2022-06-26 05:55:25 +0200

    Added loading ... while waite for data

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,23 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-    .then(res => {
-        return res.json();
-    })
-    .then(data =>{
-      setBlogs(data)
-    })
+    
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div id="loading-container" align="center"> Loading    . . .</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
