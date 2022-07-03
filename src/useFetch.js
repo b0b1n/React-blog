fb714d2dd7837aabac7078246d9902e5d8e78291
b0b1n <sohaibskious@gmail.com>
2022-07-03 12:20:20 +0200

    specified the '/' route

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,27 +4,26 @@ const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
-
   useEffect(() => {
-    fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Couldn't fetch data from the given ressource");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setData(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setIsPending(false);
-        setError(err.message);
-      });
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Couldn't fetch data from the given ressource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
   }, [url]);
 
-  return {data, isPending, error}
+  return { data, isPending, error };
 };
 
 export default useFetch;
