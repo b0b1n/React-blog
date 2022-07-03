ddeb3d43ea5e2ffd968fffb49d0a02cc7dc7e690
b0b1n <sohaibskious@gmail.com>
2022-07-03 14:37:43 +0200

    adding function handleSubmit

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,18 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("Skious");
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const blog = {title , body , author}
+    console.log(blog);
+
+  }
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title : </label>
         <input
           type="text"
